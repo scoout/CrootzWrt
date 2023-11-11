3dd0a04b847671ba5e9be98c65fb89567cc13ff7
iqbal fasya <79214502+scoout@users.noreply.github.com>
2023-11-12 05:02:21 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -30,4 +30,4 @@ sed -i '/^add_list system.ntp.server/c\add_list system.ntp.server='3.id.pool.ntp
 
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.100/g' package/base-files/files/bin/config_generate
