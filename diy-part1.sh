66f3c7e238cd3f68bd8d2e18c888ed42e86c8415
iqbal fasya <79214502+scoout@users.noreply.github.com>
2023-11-12 04:14:50 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git modemfeed https://github.com/scoout/modemfeed.git' >>feeds.conf.default
+echo 'src-git passwall-package https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
+echo 'src-git modemfeed https://github.com/scoout/modemfeed' >>feeds.conf.default
