ac084bbb66bb0f5a0bd275a7b8f3d4ec7d967125
iqbal fasya <79214502+scoout@users.noreply.github.com>
2023-11-17 17:48:51 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,9 +15,9 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git emortal https://github.com/scoout/immortalwrt.git' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
-echo 'src-git passpackage https://github.com/xiaorouji/openwrt-passwall-packages.git' >>feeds.conf.default
-echo 'src-git modemfeed https://github.com/koshev-msk/modemfeed.git' >>feeds.conf.default
-echo 'src-git OpenClash https://github.com/vernesong/OpenClash.git' >> feeds.conf.default
-echo 'src-git Rooter https://github.com/scoout/ROOterSource2305.git' >>feeds.conf.default
+#echo 'src-git emortal https://github.com/scoout/immortalwrt.git' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
+#echo 'src-git passpackage https://github.com/xiaorouji/openwrt-passwall-packages.git' >>feeds.conf.default
+#echo 'src-git modemfeed https://github.com/koshev-msk/modemfeed.git' >>feeds.conf.default
+#echo 'src-git OpenClash https://github.com/vernesong/OpenClash.git' >> feeds.conf.default
+#echo 'src-git Rooter https://github.com/scoout/ROOterSource2305.git' >>feeds.conf.default
